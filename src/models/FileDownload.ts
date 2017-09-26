5b20345d47f0339f605bb5d3d9d64ae6ed122cab
Shaun Burdick <shaun.burdick@swimlane.com>
2017-09-26 11:33:00 -0400

    revert addignment back to old school method

--- src/models/FileDownload.ts
+++ src/models/FileDownload.ts
@@ -4,12 +4,32 @@
 export class FileDownload {
 
   /**
+   * The download stream.
+   */
+  file: any;
+
+  /**
+   * File name for download.
+   */
+  fileName: string;
+
+  /**
+   * File mime type.
+   */
+  mimeType: string;
+
+  /**
    * Creates an instance of FileDownload.
    *
-   * @param {*} file any type that is returnable by your webserver (string, Buffer, Stream, etc)
-   * @param {string} fileName The name of the file
-   * @param {string} mimeType The mime type of the file
+   * @param {*} _file any type that is returnable by your webserver (string, Buffer, Stream, etc)
+   * @param {string} _fileName The name of the file
+   * @param {string} _mimeType The mime type of the file
    * @memberof FileDownload
    */
-  constructor(public file: any, public fileName: string, public mimeType: string) { }
+  constructor(_file: any, _fileName: string, _mimeType: string) {
+    // old school assignment until tswag supports constructor documentation
+    this.file = _file;
+    this.fileName = _fileName;
+    this.mimeType = _mimeType;
+   }
 }
